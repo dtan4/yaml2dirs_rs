ce3f99304bb416503dbd61b541cf6bcfee490980
Daisuke Fujita <dtanshi45@gmail.com>
2021-09-19 23:34:39 +0900

    Handle CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    args.next();
+
+    let filename = match args.next() {
+        Some(v) => v,
+        None => {
+            eprintln!("YAML file is required");
+            process::exit(1);
+        }
+    };
+
+    let root_dir = match env::current_dir() {
+        Ok(v) => v.into_os_string().into_string().unwrap(),
+        Err(e) => {
+            eprintln!("cannot get working directory: {}", e);
+            process::exit(1);
+        }
+    };
+
+    println!("{}", filename);
+    println!("{}", root_dir);
 }
