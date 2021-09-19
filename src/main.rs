5283ba08d1d4a51a04823c340b0dc6a59212f297
Daisuke Fujita <dtanshi45@gmail.com>
2021-09-20 01:01:39 +0900

    Parse YAML file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::fs;
 use std::process;
 
-fn parse_yaml_file(filename: String) -> Result<String, Box<dyn Error>> {
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DirTree(HashMap<String, Option<DirTree>>);
+
+fn parse_yaml_file(filename: String) -> Result<DirTree, Box<dyn Error>> {
     let body = fs::read_to_string(filename)?;
 
-    Ok(body)
+    let dir_tree: DirTree = serde_yaml::from_str(body.as_str())?;
+
+    Ok(dir_tree)
 }
 
 fn main() {
@@ -23,13 +31,15 @@ fn main() {
 
     println!("{}", filename);
 
-    let body = match parse_yaml_file(filename) {
+    let dir_tree = match parse_yaml_file(filename) {
         Ok(v) => v,
         Err(e) => {
             eprintln!("cannot parse the given file: {}", e);
             process::exit(1);
         }
     };
+
+    println!("{:?}", dir_tree);
 
     let root_dir = match env::current_dir() {
         Ok(v) => v.into_os_string().into_string().unwrap(),
@@ -39,6 +49,5 @@ fn main() {
         }
     };
 
-    println!("{}", body);
     println!("{}", root_dir);
 }
